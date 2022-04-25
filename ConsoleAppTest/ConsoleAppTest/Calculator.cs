318c8a15bc83fdd400536732eda346797d8e0a0c
Annabel <72205175+annabelspain@users.noreply.github.com>
2022-04-25 08:53:46 +0100

    Add files via upload

---
+++ ConsoleAppTest/ConsoleAppTest/Calculator.cs
@@ -0,0 +1,58 @@
+﻿
+
+public class Calculator
+{
+    private float answer;
+
+    public float GetAnswer()
+    {
+        return answer;
+    }
+
+    public void Calculation(double firstNumber, double secondNumber, string operator1, float answer)
+    {
+        float answer; 
+
+        switch (operator1)
+        {
+            case "+":
+                answer = (float)(firstNumber + secondNumber);
+                break;
+            case "-":
+                answer = (float)(firstNumber - secondNumber);
+                break;
+            case "*":
+                answer = (float)(firstNumber * secondNumber);
+                break;
+            case "/":
+                answer = (float)(firstNumber / secondNumber);
+                break;
+        }
+        Console.WriteLine($"Result is {answer}");
+    }
+}
+
+//internal void Calculation(double firstNumber, double secondNumber, string operator1)
+//{
+//    Calculation(firstNumber, secondNumber, operator1, answer);
+//}
+
+//public void Calculation(double num1, double num2, string operator1, double answer)
+//{
+//    switch (operator1)
+//    {
+//        case "+":
+//            answer = num1 + num2;
+//            break;
+//        case "-":
+//            answer = num1 - num2;
+//            break;
+//        case "*":
+//            answer = num1 * num2;
+//            break;
+//        case "/":
+//            answer = num1 / num2;
+//            break;
+//    }
+//    Console.WriteLine($"Result is {answer}");
+//}
