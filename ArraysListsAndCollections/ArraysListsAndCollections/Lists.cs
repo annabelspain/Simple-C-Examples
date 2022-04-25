7186aaaa27b4c83a8d2782ca9273392b0007ebdd
Annabel <72205175+annabelspain@users.noreply.github.com>
2022-04-25 08:52:53 +0100

    Add files via upload

---
+++ ArraysListsAndCollections/ArraysListsAndCollections/Lists.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysListsAndCollections
+{
+    class Lists
+    {
+        //static void Main(string[] args)
+        //{
+        //    var fibonacciNumbers = new List<int> { 1, 1 };
+
+        //    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+        //    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+        //    fibonacciNumbers.Add(previous + previous2);
+
+        //    foreach (var item in fibonacciNumbers)
+        //        Console.WriteLine(item);
+        //}
+
+    }
+}
